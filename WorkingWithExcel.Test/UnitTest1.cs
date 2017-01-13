7cfda7595785065b2ffb25d12f86f3f9e4c23fe9
Kcsorensen <kcsorensen@outlook.dk>
2017-01-13 22:45:26 +0100

    Installed EPPLus.Core to created excel files

---
+++ WorkingWithExcel.Test/UnitTest1.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WorkingWithExcel.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //Excel.Application xlApp = new Excel.Application();
+
+            //if (xlApp == null)
+            //{
+            //    throw new NullReferenceException("Excel is not properly installed!");
+            //}
+
+            //Excel.Workbook xlWorkBook;
+            //Excel.Worksheet xlWorkSheet;
+            //object misValue = System.Reflection.Missing.Value;
+
+            //xlWorkBook = xlApp.Workbooks.Add(misValue);
+            //xlWorkSheet = (Excel.Worksheet) xlWorkBook.Worksheets.get_Item(1);
+
+            //xlWorkSheet.Cells[1, 1] = "ID";
+            //xlWorkSheet.Cells[1, 2] = "Name";
+            //xlWorkSheet.Cells[2, 1] = "1";
+            //xlWorkSheet.Cells[2, 2] = "One";
+            //xlWorkSheet.Cells[3, 1] = "2";
+            //xlWorkSheet.Cells[3, 2] = "Two";
+
+            //xlWorkBook.SaveAs("d:\\csharp-Excel.xls", Excel.XlFileFormat.xlWorkbookNormal, misValue, misValue, misValue, misValue, Excel.XlSaveAsAccessMode.xlExclusive, misValue, misValue, misValue, misValue, misValue);
+            //xlWorkBook.Close(true, misValue, misValue);
+            //xlApp.Quit();
+
+            //Assert.IsTrue(File.Exists("d:\\csharp-Excel.xls"));
+        }
+    }
+}
--- src/FireSafetyTools/Controllers/DesignFireController.cs
+++ src/FireSafetyTools/Controllers/DesignFireController.cs
@@ -1,3 +1,4 @@
+using System.IO;
 using System.Threading.Tasks;
 using FireSafetyTools.Dtos.Tools.FireSafety;
 using FireSafetyTools.Models.Tools.FireSafety.DesignFire;
@@ -5,11 +6,55 @@ using FireSafetyTools.Services;
 using FireSafetyTools.ViewModels.Tools.FireSafety.DesignFire;
 using Microsoft.AspNetCore.Mvc;
 using System.Linq;
+using Microsoft.AspNetCore.Hosting;
+using OfficeOpenXml;
 
 namespace FireSafetyTools.Controllers
 {
     public class DesignFireController : Controller
     {
+        public DesignFireController()
+        {
+            string sFileName = @"d:\\demo.xlsx";
+            
+            FileInfo file = new FileInfo(Path.Combine(sFileName));
+
+            if (file.Exists)
+            {
+                file.Delete();
+            }
+
+            using (ExcelPackage package = new ExcelPackage(file))
+            {
+                // add a new worksheet to the empty workbook
+                ExcelWorksheet worksheet = package.Workbook.Worksheets.Add("Employee");
+                //First add the headers
+                worksheet.Cells[1, 1].Value = "ID";
+                worksheet.Cells[1, 2].Value = "Name";
+                worksheet.Cells[1, 3].Value = "Gender";
+                worksheet.Cells[1, 4].Value = "Salary (in $)";
+
+                //Add values
+                worksheet.Cells["A2"].Value = 1000;
+                worksheet.Cells["B2"].Value = "Jon";
+                worksheet.Cells["C2"].Value = "M";
+                worksheet.Cells["D2"].Value = 5000;
+
+                worksheet.Cells["A3"].Value = 1001;
+                worksheet.Cells["B3"].Value = "Graham";
+                worksheet.Cells["C3"].Value = "M";
+                worksheet.Cells["D3"].Value = 10000;
+
+                worksheet.Cells["A4"].Value = 1002;
+                worksheet.Cells["B4"].Value = "Jenny";
+                worksheet.Cells["C4"].Value = "F";
+                worksheet.Cells["D4"].Value = 5000;
+
+                package.Save(); //Save the workbook.
+
+            }
+        }
+
         public IActionResult Index()
         {
             if (HttpContext.Session.GetObjectFromJson<DesignFireViewModel>(SessionNames.DesignFireData) == null)
